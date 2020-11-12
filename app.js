7fd2febda912bcd5476a126259e695ed81c2e1fc
kana kozaka <kana@kananoMacBook-Pro.local>
2020-11-12 16:06:26 +0900

    [Start]JavaScript4章jQueryを使ってみよう

---
+++ app.js
@@ -0,0 +1,46 @@
+// $(document).ready(function(){
+//   $('body').html('<h1>Hello jQuery!!</h1>');
+// });
+
+// $(function(){
+//   $('.box1').css({
+//     'background-color': '#0000FF',
+//     'height': '100px' 
+//   });
+// });
+
+// $(function(){
+//   $('.box1').on('click',function(){
+//     $('.box1').addClass('box1-ext');
+//   });
+//   $('.box1').mouseout(function(){
+//     $('.box1').removeClass('box1-ext');
+//   });
+// });
+
+// $(function(){
+//   $('.bg1').on('click',function(){
+//     $('.bg1').slideUp();
+//   });
+//   $('.bg2').on('click',function(){
+//     $('.bg2').slideUp();
+//   });
+//   $('.bg3').on('click',function(){
+//     $('.bg3').slideUp();
+//   });
+//   $('.bg4').on('click',function(){
+//     $('.bg4').slideUp();
+//   });
+// });
+
+// $(function(){
+//   $('.box1').on('click',function(){
+//     $(this).slideUp();
+//   });
+// });
+
+$(function(){
+  $('button').on('click',function(){
+    $('ul').children().css('color','red');
+  });
+});
